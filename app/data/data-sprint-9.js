b7e5652b50f4a80834df3bf3d2e361c36f226357
Ming Chan <mingosoft@gmail.com>
2026-09-22 18:04:50 +0100

    The first SC record is now clickable

--- app/data/data-sprint-9.js
+++ app/data/data-sprint-9.js
@@ -1,26 +1,26 @@
 module.exports = {
   visits: [
-    { reference: 'VST-20260825-008', date: '25 August 2026', sortDate: '2026-08-25', organisations: ['United Learning Trust', 'St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River' },
-    { reference: 'VST-20260818-012', date: '18 August 2026', sortDate: '2026-08-18', organisations: ['Hammersmith and Fulham'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith' },
-    { reference: 'VST-20260817-010', date: '17 August 2026', sortDate: '2026-08-17', organisations: ['United Learning Trust', 'Hammersmith and Fulham', 'St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park' },
-    { reference: 'VST-20260817-006', date: '17 August 2026', sortDate: '2026-08-17', organisations: ['St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River' },
-    { reference: 'VST-20260805-003', date: '5 August 2026', sortDate: '2026-08-05', organisations: ['Hammersmith and Fulham', 'St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith' },
-    { reference: 'VST-20260805-001', date: '5 August 2026', sortDate: '2026-08-05', organisations: ['United Learning Trust'], status: 'New', tag: 'govuk-tag--grey', recorder: 'Jo Park' },
-    { reference: 'VST-20260706-001', date: '6 July 2026', sortDate: '2026-07-06', organisations: ['St George\'s Primary School', 'Hammersmith and Fulham'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River' },
-    { reference: 'VST-20260615-005', date: '15 June 2026', sortDate: '2026-06-15', organisations: ['Hammersmith and Fulham'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith' },
-    { reference: 'VST-20260612-030', date: '12 June 2026', sortDate: '2026-06-12', organisations: ['United Learning Trust', 'St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park' },
-    { reference: 'VST-20260612-028', date: '12 June 2026', sortDate: '2026-06-12', organisations: ['Hammersmith and Fulham', 'United Learning Trust'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River' }
+    { reference: 'VST-20260825-008', date: '25 August 2026', sortDate: '2026-08-25', organisations: ['United Learning Trust', 'St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River', link: '#' },
+    { reference: 'VST-20260818-012', date: '18 August 2026', sortDate: '2026-08-18', organisations: ['Hammersmith and Fulham'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith', link: '#' },
+    { reference: 'VST-20260817-010', date: '17 August 2026', sortDate: '2026-08-17', organisations: ['United Learning Trust', 'Hammersmith and Fulham', 'St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park', link: '#' },
+    { reference: 'VST-20260817-006', date: '17 August 2026', sortDate: '2026-08-17', organisations: ['St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River', link: '#' },
+    { reference: 'VST-20260805-003', date: '5 August 2026', sortDate: '2026-08-05', organisations: ['Hammersmith and Fulham', 'St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith', link: '#' },
+    { reference: 'VST-20260805-001', date: '5 August 2026', sortDate: '2026-08-05', organisations: ['United Learning Trust'], status: 'New', tag: 'govuk-tag--grey', recorder: 'Jo Park', link: '#' },
+    { reference: 'VST-20260706-001', date: '6 July 2026', sortDate: '2026-07-06', organisations: ['St George\'s Primary School', 'Hammersmith and Fulham'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River', link: '#' },
+    { reference: 'VST-20260615-005', date: '15 June 2026', sortDate: '2026-06-15', organisations: ['Hammersmith and Fulham'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith', link: '#' },
+    { reference: 'VST-20260612-030', date: '12 June 2026', sortDate: '2026-06-12', organisations: ['United Learning Trust', 'St George\'s Primary School'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park', link: '#' },
+    { reference: 'VST-20260612-028', date: '12 June 2026', sortDate: '2026-06-12', organisations: ['Hammersmith and Fulham', 'United Learning Trust'], status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River', link: '#' }
   ],
   strategicConversations: [
-    { reference: 'SC-20260902-017', date: '2 September 2026', sortDate: '2026-09-02', trust: 'United Learning Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park' },
-    { reference: 'SC-20260828-014', date: '28 August 2026', sortDate: '2026-08-28', trust: 'AET Academy Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith' },
-    { reference: 'SC-20260821-009', date: '21 August 2026', sortDate: '2026-08-21', trust: 'Leeds Diocesan Learning Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River' },
-    { reference: 'SC-20260819-011', date: '19 August 2026', sortDate: '2026-08-19', trust: 'Harris Federation', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park' },
-    { reference: 'SC-20260813-005', date: '13 August 2026', sortDate: '2026-08-13', trust: 'United Learning Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith' },
-    { reference: 'SC-20260808-003', date: '8 August 2026', sortDate: '2026-08-08', trust: 'Coventry Academy Trust', status: 'New', tag: 'govuk-tag--grey', recorder: 'Alex River' },
-    { reference: 'SC-20260730-004', date: '30 July 2026', sortDate: '2026-07-30', trust: 'Dixons Academies Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park' },
-    { reference: 'SC-20260722-006', date: '22 July 2026', sortDate: '2026-07-22', trust: 'The Education Fellowship', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith' },
-    { reference: 'SC-20260715-001', date: '15 July 2026', sortDate: '2026-07-15', trust: 'AET Academy Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River' },
-    { reference: 'SC-20260709-002', date: '9 July 2026', sortDate: '2026-07-09', trust: 'United Learning Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park' }
+    { reference: 'SC-20260902-017', date: '2 September 2026', sortDate: '2026-09-02', trust: 'United Learning Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park', link: '/sprint-9/task-list-delivery-officers-combined-strategic-conversation-existing.html' },
+    { reference: 'SC-20260828-014', date: '28 August 2026', sortDate: '2026-08-28', trust: 'AET Academy Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith', link: '#' },
+    { reference: 'SC-20260821-009', date: '21 August 2026', sortDate: '2026-08-21', trust: 'Leeds Diocesan Learning Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River', link: '#' },
+    { reference: 'SC-20260819-011', date: '19 August 2026', sortDate: '2026-08-19', trust: 'Harris Federation', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park', link: '#' },
+    { reference: 'SC-20260813-005', date: '13 August 2026', sortDate: '2026-08-13', trust: 'United Learning Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith', link: '#' },
+    { reference: 'SC-20260808-003', date: '8 August 2026', sortDate: '2026-08-08', trust: 'Coventry Academy Trust', status: 'New', tag: 'govuk-tag--grey', recorder: 'Alex River', link: '#' },
+    { reference: 'SC-20260730-004', date: '30 July 2026', sortDate: '2026-07-30', trust: 'Dixons Academies Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park', link: '#' },
+    { reference: 'SC-20260722-006', date: '22 July 2026', sortDate: '2026-07-22', trust: 'The Education Fellowship', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Chris Smith', link: '#' },
+    { reference: 'SC-20260715-001', date: '15 July 2026', sortDate: '2026-07-15', trust: 'AET Academy Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Alex River', link: '#' },
+    { reference: 'SC-20260709-002', date: '9 July 2026', sortDate: '2026-07-09', trust: 'United Learning Trust', status: 'In progress', tag: 'govuk-tag--blue', recorder: 'Jo Park', link: '#' }
   ]
 }
